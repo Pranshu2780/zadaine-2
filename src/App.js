9e95488906b3ceb5d2cd7a40361a1ef74c97289d
Milind Pandey (Captain Levi) <47604864+Pranshu2780@users.noreply.github.com>
2021-02-07 16:07:48 +0530

    initialize

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-function App() {
+const Todos=()=>{
+  const dispatch = useDispatch();
+  const todos=useSelector(state=>state.todos);
+  const handleClick = id=>dispatch({
+    type: 'DELETE_TODO',
+    payload:id,
+  })
+  if(!todos || !todos.length ){
+     return <p>
+       Todo-List is Empty
+     </p>
+  }
+  return(
+    <ul>
+      {todos.map(todo=><li onClick={()=>handleClick(todo.id) } > {todo.label}</li>)}
+    </ul>
+  )
+};
+
+const TodoInput=()=>{
+  const dispatch = useDispatch();
+  const [newTodo, setNewTodo] = useState();
+  const handleChange = event=>{
+       setNewTodo(event.target.value);
+  }
+  const handleClick = ()=>dispatch({
+    type:'ADD_TODO',
+    payload:{
+      label:newTodo,
+      id: Math.ceil(Math.random()*100),
+    }
+  });
+  return(
+    <div>
+    <input value={newTodo}  onChange={handleChange} type="text" />
+    <button onClick={handleClick} > ADD</button>
+ 
+    </div>
+     )
+};
+
+class App extends React.Component {
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>
+        TODO-LIST
+      </p>
+      <Todos/>
+      <TodoInput />
+      {/* <header>
+        <form>
+          <input placeholder="Enter the text" id="input" />
+          <button type="submit" >Add</button>
+        </form>
+      </header> */}
     </div>
   );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,29 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+import { Provider } from 'react-redux';
+import { createStore } from 'redux' ;
+import reducer from './reducer';
+
+const store = createStore(reducer); 
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store = {store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  
+   document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
